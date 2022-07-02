4d92c54592cc1253713d4f1648cf88e6e1cd3a58
amitn16 <amitnmj@gmail.com>
2022-07-02 22:03:51 +0200

    update

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "google" {
   project     = "${var.var_project}"
 }
 module "vpc" {
-  source = "../modules/global" 
+  source = "./modules/global" 
   env                   = "${var.var_env}"
   company               = "${var.var_company}"
   var_uc1_public_subnet = "${var.uc1_public_subnet}"
@@ -11,7 +11,7 @@ module "vpc" {
   var_ue1_private_subnet= "${var.ue1_private_subnet}"
 }
 module "uc1" {
-  source                = "../modules/uc1"
+  source                = "./modules/uc1"
   network_self_link     = "${module.vpc.out_vpc_self_link}"
   subnetwork1           = "${module.uc1.uc1_out_public_subnet_name}"
   env                   = "${var.var_env}"
@@ -20,7 +20,7 @@ module "uc1" {
   var_uc1_private_subnet= "${var.uc1_private_subnet}"
 }
 module "ue1" {
-  source                = "../modules/ue1"
+  source                = "./modules/ue1"
   network_self_link     = "${module.vpc.out_vpc_self_link}"
   subnetwork1           = "${module.ue1.ue1_out_public_subnet_name}"
   env                   = "${var.var_env}"
