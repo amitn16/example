d6c2a9225f965480ae29415874c08d47f1cd4a2a
Amit <amitnmj@gmail.com>
2022-07-02 12:53:20 +0000

    use vcs

---
+++ main.tf
@@ -0,0 +1,26 @@
+provider "google" {
+  project = "my-project-lab1-351507"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "e2-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    # A default network is created for all GCP projects
+    network = "default"
+    access_config {
+    }
+  }
+}
+resource "google_compute_network" "vpc_network" {
+  name                    = "terraform-network"
+  auto_create_subnetworks = "true"
+}
