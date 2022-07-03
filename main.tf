ffa05c6aea10f7122543a82cb62f757928bf6ac8
amitn16 <amitnmj@gmail.com>
2022-07-03 19:25:46 +0200

    ok

--- main.tf
+++ main.tf
@@ -3,12 +3,7 @@ provider "google" {
   region  = var.region
 }
 
-resource "google_project" "project" {
-  name       = "My Project"
-  project = "my-project-lab1-351507"
-  }
-
 resource "google_app_engine_application" "app" {
-  project     = google_project.project.project
-  location_id = "us-central"
-}
+  project = var.project
+  region = var.region 
+  }
