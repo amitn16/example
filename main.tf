bf51c394835d5adc9ff0157345895989483385de
amitn16 <amitnmj@gmail.com>
2022-07-03 13:01:42 +0200

    startup

--- main.tf
+++ main.tf
@@ -86,10 +86,6 @@ resource "google_compute_instance" "dev" {
  #  ssh-keys = "${var.user}:${file(var.publickeypath)}"
  # }
 metadata {
-        startup-script = <<SCRIPT
-        sudo yum -y install epel-release
-        sudo yum -y install nginx
-        sudo nginx -v
-        SCRIPT
+        startup-script = startup.sh
     }
 }
---
+++ startup.sh
@@ -0,0 +1,3 @@
+sudo yum -y install epel-release
+sudo yum -y install ngin
+sudo nginx -v
