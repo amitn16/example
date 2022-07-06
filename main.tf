90899a06ee6c3530ca7e1b4063109f1e5e27024c
amitn16 <amitnmj@gmail.com>
2022-07-06 22:45:38 +0200

    ok

--- main.tf
+++ main.tf
@@ -2,108 +2,35 @@ provider "google" {
   project = var.project
   region  = var.region
 }
-
-/*
-resource "google_project" "my_project" {
-  name       = "My Project"
-  project_id = "my-project-lab2-351508"
-  org_id     = "277860026539"
+resource "google_storage_bucket" "bucket" {
+  name     = "test-bucket"
+  location = "US"
 }
 
-*/
-resource "google_project" "my_project-in-a-folder" {
-  name       = "My Project 1"
-  project_id = "my-project-lab3-351509"
-  folder_id  = google_folder.BU2.name
-  }
-
-resource "google_folder" "BU2" {
-  display_name = "BU2"
-  parent       = "organizations/277860026539"
+resource "google_storage_bucket_object" "archive" {
+  name   = "index.zip"
+  bucket = google_storage_bucket.bucket.name
+  source = "./code"
 }
 
-/*
-resource "google_compute_firewall" "firewall" {
-  name    = "firewall-externalssh"
-  network = "default"
+resource "google_cloudfunctions_function" "function" {
+  name        = "function-test"
+  description = "My function"
+  runtime     = "nodejs16"
 
-  allow {
-    protocol = "tcp"
-    ports    = ["22"]
-  }
-
-  source_ranges = ["0.0.0.0/0"] # Not So Secure. Limit the Source Range
-  target_tags   = ["externalssh"]
+  available_memory_mb   = 128
+  source_archive_bucket = google_storage_bucket.bucket.name
+  source_archive_object = google_storage_bucket_object.archive.name
+  trigger_http          = true
+  entry_point           = "helloGET"
 }
 
-resource "google_compute_firewall" "webserverrule" {
-  name    = "amit-webserver"
-  network = "default"
+# IAM entry for all users to invoke the function
+resource "google_cloudfunctions_function_iam_member" "invoker" {
+  project        = google_cloudfunctions_function.function.project
+  region         = google_cloudfunctions_function.function.region
+  cloud_function = google_cloudfunctions_function.function.name
 
-  allow {
-    protocol = "tcp"
-    ports    = ["80","443"]
-  }
-
-  source_ranges = ["0.0.0.0/0"] # Not So Secure. Limit the Source Range
-  target_tags   = ["webserver"]
+  role   = "roles/cloudfunctions.invoker"
+  member = "allUsers"
 }
-
-# We create a public IP address for our google compute instance to utilize
-resource "google_compute_address" "static" {
-  name = "vm-public-address"
-  project = var.project
-  region = var.region
-  depends_on = [ google_compute_firewall.firewall ]
-}
-
-
-resource "google_compute_instance" "dev" {
-  name         = var.name
-  machine_type = "e2-micro"
-  zone         = "${var.region}-a"
-  tags         = ["externalssh","webserver"]
-
-  boot_disk {
-    initialize_params {
-      image = "centos-cloud/centos-7"
-    }
-  }
-
-  network_interface {
-    network = "default"
-
-    access_config {
-      nat_ip = google_compute_address.static.address
-    }
-  }
-
- provisioner "remote-exec" {
-    connection {
-      host        = google_compute_address.static.address
-      type        = "ssh"
-      user        = var.user
-      timeout     = "500s"
-      private_key = file(var.privatekeypath)
-    }
-
-    inline = [
-      "sudo yum -y install epel-release",
-      "sudo yum -y install nginx",
-      "sudo nginx -v",
-    ]
-  }
-
-  # Ensure firewall rule is provisioned before server, so that SSH doesn't fail.
-  depends_on = [ google_compute_firewall.firewall, google_compute_firewall.webserverrule ]
-
-  service_account {
-    email  = var.email
-    scopes = ["compute-ro"]
-  }
-
- # metadata = {
- #  ssh-keys = "${var.user}:${file(var.publickeypath)}"
- # }
-metadata_startup_script = file("${path.module}/startup.sh")
-*/
