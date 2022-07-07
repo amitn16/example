8dc13cc6f13103c5e428b106aea7b13a0b1d5421
amitn16 <amitnmj@gmail.com>
2022-07-07 19:42:50 +0200

    ok

--- main.tf
+++ main.tf
@@ -2,3 +2,40 @@ provider "google" {
   project = var.project
   region  = var.region
 }
+
+resource "google_storage_bucket" "bucket" {
+  name = "terraform-gcp-amit121201"
+}
+
+resource "google_storage_bucket_object" "archive" {
+  name   = "index.zip"
+  bucket = google_storage_bucket.bucket.name
+  source = "function.zip"
+}
+
+resource "google_cloudfunctions_function" "function" {
+  name        = "function-test"
+  description = "My function"
+  runtime     = "nodejs10"
+
+  available_memory_mb   = 128
+  source_archive_bucket = google_storage_bucket.bucket.name
+  source_archive_object = google_storage_bucket_object.archive.name
+  trigger_http          = true
+  timeout               = 60
+  entry_point           = "helloWorld"
+  labels = {
+    my-label = "my-label-value"
+  }
+
+  environment_variables = {
+    MY_ENV_VAR = "my-env-var-value"
+  }
+}
+
+# IAM entry for a single user to invoke the function
+resource "google_cloudfunctions_function_iam_member" "invoker" {
+ cloud_function = google_cloudfunctions_function.function.name
+
+  role   = "roles/cloudfunctions.invoker"
+  member = "allUsers"
