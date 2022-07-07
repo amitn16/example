7e5dea3a72ebf42625b408736d16694bd952dd64
amitn16 <amitnmj@gmail.com>
2022-07-07 12:41:15 +0200

    ok

--- main.tf
+++ main.tf
@@ -19,8 +19,6 @@ resource "google_cloudfunctions_function" "function" {
   runtime     = "nodejs16"
 
   available_memory_mb   = 128
-  source_archive_bucket = google_storage_bucket.bucketamit2203.name
-  source_archive_object = google_storage_bucket_object.archive.name
   trigger_http          = true
   entry_point           = "helloGET"
 }
