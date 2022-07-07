d432920ecca8d00a6a7a789dbb40a5ae836898f7
amitn16 <amitnmj@gmail.com>
2022-07-07 06:41:37 +0200

    ok

---
+++ terraform/main.tf
@@ -0,0 +1,22 @@
+provider "google" {
+  project = var.project
+  region  = var.region
+}
+
+resource "google_cloudfunctions_function" "function" {
+  name        = "function-test"
+  description = "My function"
+  runtime     = "nodejs16"
+
+  available_memory_mb   = 128
+}
+
+# IAM entry for all users to invoke the function
+resource "google_cloudfunctions_function_iam_member" "invoker" {
+  project        = google_cloudfunctions_function.function.project
+  region         = google_cloudfunctions_function.function.region
+  cloud_function = google_cloudfunctions_function.function.name
+
+  role   = "roles/cloudfunctions.invoker"
+  member = "allUsers"
+}
