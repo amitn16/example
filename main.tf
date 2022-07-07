14a21f122c62ded2443baec702ea7f82a3fc6e8d
amitn16 <amitnmj@gmail.com>
2022-07-07 12:28:17 +0200

    ok

--- main.tf
+++ main.tf
@@ -13,3 +13,24 @@ resource "google_storage_bucket_object" "archive" {
   bucket = google_storage_bucket.bucketamit2203.name
   source = "./code/function.zip"
 }
+resource "google_cloudfunctions_function" "function" {
+  name        = "function-test"
+  description = "My function"
+  runtime     = "nodejs16"
+
+  available_memory_mb   = 128
+  source_archive_bucket = google_storage_bucket.bucketamit2203.name
+  source_archive_object = google_storage_bucket_object.archive.name
+  trigger_http          = true
+  entry_point           = "helloGET"
+}
+
+# IAM entry for all users to invoke the function
+resource "google_cloudfunctions_function_iam_member" "invoker" {
+  project        = google_cloudfunctions_function.function.project
+  region         = google_cloudfunctions_function.function.region
+  cloud_function = google_cloudfunctions_function.function.name
+
+  role   = "roles/cloudfunctions.invoker"
+  member = "allUsers"
+}
