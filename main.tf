6c056000dd7baf1349a4c785d7b53b22ca79c63b
amitn16 <amitnmj@gmail.com>
2022-07-08 14:34:25 +0200

    ok

--- main.tf
+++ main.tf
@@ -3,15 +3,6 @@ provider "google" {
   region  = var.region
   }
 
-resource "google_project_service" "project" {
-  project = "project-lab1-351507"
-  service = "serviceusage.googleapis.com"
-
-  disable_dependent_services = false
-}
-
-
-
 resource "google_cloud_run_service" "nginx-service-1" {
   name     = "nginx-service-my-project-lab1-351507"
   location = "europe-west3"
