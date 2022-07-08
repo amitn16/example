ad975ca344940ace08886820e34cbf07b37f7d6d
amitn16 <amitnmj@gmail.com>
2022-07-08 19:48:07 +0200

    ok

--- main.tf
+++ main.tf
@@ -3,18 +3,6 @@ provider "google" {
   region  = var.region
   }
 
-module "project-services" {
-  source  = "terraform-google-modules/project-factory/google//modules/project_services"
-  version = "10.1.1"
-
-  project_id                  = "project-lab1-351507"
-
-  activate_apis = [
-    "serviceusage.googleapis.com",
-  ]
-}
-
-
 resource "google_cloud_run_service" "default" {
   name     = "cloudrun-srv"
   location = "us-central1"
