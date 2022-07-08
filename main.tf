96ddfc6596b52bd2612f0c9d0e0035d8db361f4a
amitn16 <amitnmj@gmail.com>
2022-07-08 20:00:25 +0200

    ok

--- main.tf
+++ main.tf
@@ -7,5 +7,5 @@ resource "google_project_service" "project" {
   project = var.project
   service = "serviceusage.googleapis.com"
 
-  disable_dependent_services = true
+  disable_dependent_services = false
 }
