844d999bac238d471e775d5cd27a44767756d9aa
amitn16 <amitnmj@gmail.com>
2022-07-08 20:07:01 +0200

    ok

--- main.tf
+++ main.tf
@@ -2,10 +2,3 @@ provider "google" {
   project = var.project
   region  = var.region
   }
-
-resource "google_project_service" "project" {
-  project = var.project
-  service = "serviceusage.googleapis.com"
-
-  disable_dependent_services = false
-}
