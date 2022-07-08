19b19de6c731b4bc72ec16742c6efa0567d2bd27
amitn16 <amitnmj@gmail.com>
2022-07-08 14:47:26 +0200

    ok

--- main.tf
+++ main.tf
@@ -3,6 +3,20 @@ provider "google" {
   region  = var.region
   }
 
+module "project-services" {
+  source  = "terraform-google-modules/project-factory/google//modules/project_services"
+  version = "10.1.1"
+
+  project_id                  = "project-lab1-351507"
+
+  activate_apis = [
+    "compute.googleapis.com",
+    "iam.googleapis.com",
+    "serviceusage.googleapis.com",
+  ]
+}
+
+
 resource "google_cloud_run_service" "nginx-service-1" {
   name     = "nginx-service-my-project-lab1-351507"
   location = "europe-west3"
