4307067696a10158e3509341b59df58b726ae596
amitn16 <amitnmj@gmail.com>
2022-07-09 00:10:31 +0200

    ok

--- main.tf
+++ main.tf
@@ -3,21 +3,32 @@ provider "google" {
   region  = var.region
   }
 
-resource "google_cloud_run_service" "nginx-service" {
-  name = "${var.environment}-nginx-service"
-  location = "europe-west3"
+resource "google_cloud_run_service" "default" {
+  name     = "cloudrun-srv"
+  location = "us-central1"
+
   template {
     spec {
       containers {
-        image = "marketplace.gcr.io/google/nginx1"
-        ports {
-          container_port = 80
-        }
+        image = "us-docker.pkg.dev/cloudrun/container/hello"
       }
     }
   }
-  traffic {
-    percent = 100
-    latest_revision = true
+}
+
+data "google_iam_policy" "noauth" {
+  binding {
+    role = "roles/run.invoker"
+    members = [
+      "allUsers",
+    ]
   }
+}
+
+resource "google_cloud_run_service_iam_policy" "noauth" {
+  location    = google_cloud_run_service.default.location
+  project     = google_cloud_run_service.default.project
+  service     = google_cloud_run_service.default.name
+
+  policy_data = data.google_iam_policy.noauth.policy_data
 }
