fffc34e6591e6f642b49fb9ba0233afbf0a185e2
amitn16 <amitnmj@gmail.com>
2022-07-08 11:05:40 +0200

    ok

--- main.tf
+++ main.tf
@@ -2,66 +2,31 @@ provider "google" {
   project = var.project
   region  = var.region
 }
-module "vpc" {
-    source  = "terraform-google-modules/network/google"
-    version = "~> 4.0"
 
-    project_id   = "my-project-lab1-351507"
-    network_name = "example-vpc"
-    routing_mode = "GLOBAL"
-
-    subnets = [
-        {
-            subnet_name           = "subnet-01"
-            subnet_ip             = "10.10.10.0/24"
-            subnet_region         = "us-west1"
-        },
-        {
-            subnet_name           = "subnet-02"
-            subnet_ip             = "10.10.20.0/24"
-            subnet_region         = "us-west1"
-            subnet_private_access = "true"
-            subnet_flow_logs      = "true"
-            description           = "This subnet has a description"
-        },
-        
-        {
-            subnet_name               = "subnet-03"
-            subnet_ip                 = "10.10.30.0/24"
-            subnet_region             = "us-west1"
-            subnet_flow_logs          = "true"
-            subnet_flow_logs_interval = "INTERVAL_10_MIN"
-            subnet_flow_logs_sampling = 0.7
-            subnet_flow_logs_metadata = "INCLUDE_ALL_METADATA"
+resource "google_cloud_run_service" "nginx-service-1" {
+  name     = "nginx-service-my-project-lab1-351507"
+  location = "europe-west3"
+  template {
+    spec {
+      containers {
+        image = "marketplace.gcr.io/google/nginx1"
+        ports {
+          container_port = 8080
         }
-    ]
-
-    secondary_ranges = {
-        subnet-01 = [
-            {
-                range_name    = "subnet-01-secondary-01"
-                ip_cidr_range = "192.168.64.0/24"
-            },
-        ]
-
-        subnet-02 = []
+      }
     }
+  }
 
-    routes = [
-        {
-            name                   = "egress-internet"
-            description            = "route through IGW to access internet"
-            destination_range      = "0.0.0.0/0"
-            tags                   = "egress-inet"
-            next_hop_internet      = "true"
-        },
-        {
-            name                   = "app-proxy"
-            description            = "route through proxy to reach app"
-            destination_range      = "10.50.10.0/24"
-            tags                   = "app-proxy"
-            next_hop_instance      = "app-proxy-instance"
-            next_hop_instance_zone = "us-west1-a"
-        },
-    ]
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+}
+
+resource "google_cloud_run_service_iam_member" "member" {
+  location = google_cloud_run_service.nginx-service.location
+  project  = google_cloud_run_service.nginx-service.project
+  service  = google_cloud_run_service.nginx-service.name
+  role     = "roles/run.invoker"
+  member   = "allUsers"
 }
