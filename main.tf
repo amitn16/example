4c39eb8325ee308eb7ba428e1d0ea625f9d3617a
amitn16 <amitnmj@gmail.com>
2022-07-08 23:00:15 +0200

    ok

--- main.tf
+++ main.tf
@@ -2,3 +2,28 @@ provider "google" {
   project = var.project
   region  = var.region
   }
+
+  resource "google_project_service" "project" {
+  project = "my-project-lab1-351507"
+  service = "serviceusage.googleapis.com"
+
+  disable_dependent_services = true
+}
+
+resource "google_cloud_run_service" "default" {
+  name     = "cloudrun-srv"
+  location = "us-central1"
+
+  template {
+    spec {
+      containers {
+        image = "us-docker.pkg.dev/cloudrun/container/hello"
+      }
+    }
+  }
+
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+}
