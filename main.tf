15768642205455a94a042b54d0e0236802a09afb
amitn16 <amitnmj@gmail.com>
2022-07-09 22:19:09 +0200

    ok

--- main.tf
+++ main.tf
@@ -4,21 +4,3 @@ provider "google" {
   
   }
 
-resource "google_secret_manager_secret" "secret-basic" {
-  secret_id = "app1"
-
-  labels = {
-    label = "my-label1"
-  }
-
-  replication {
-    user_managed {
-      replicas {
-        location = "us-central1"
-      }
-      replicas {
-        location = "us-east1"
-      }
-    }
-  }
-}
