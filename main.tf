3aa309a715064504ae1ece112bd7ecc0ed11ea9e
amitn16 <amitnmj@gmail.com>
2022-07-09 15:54:02 +0200

    ok

--- main.tf
+++ main.tf
@@ -4,13 +4,17 @@ provider "google" {
   
   }
 
-module "project-factory_example_project_services" {
-  source  = "terraform-google-modules/project-factory/google//examples/project_services"
+module "project-factory_project_services" {
+  source  = "terraform-google-modules/project-factory/google//modules/project_services"
   version = "13.0.0"
   # insert the 1 required variable here
-  project_id = var.project_id
-  }
-
+    project_id = "my-project-lab1-351507"
+  activate_apis = [
+    "compute.googleapis.com",
+    "iam.googleapis.com",
+    "serviceusage.googleapis.com",
+  ]
+}
 
 resource "google_cloud_run_service" "default" {
   name     = "cloudrun-srv"
