b433e465da3cdc9d4d3df871e1926c8c09aceff8
amitn16 <amitnmj@gmail.com>
2022-07-09 08:31:33 +0200

    ok

--- variables.tf
+++ variables.tf
@@ -12,3 +12,14 @@ variable "environment" {
     type = string
     default = "dev"
 }
+
+variable "project_id" {
+  description = "The GCP project you want to enable APIs on"
+    type = string
+    default = "my-project-lab1-351507"
+}
+
+variable "enable" {
+  description = "Actually enable the APIs listed"
+  default     = true
+}
