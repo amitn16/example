7ff897de7e0f7ec959c85e20f26963e33705c910
amitn16 <amitnmj@gmail.com>
2022-07-09 08:38:28 +0200

    ok

--- main.tf
+++ main.tf
@@ -7,21 +7,11 @@ module "project-factory_example_project_services" {
   source  = "terraform-google-modules/project-factory/google//examples/project_services"
   version = "13.0.0"
   # insert the 1 required variable here
-  project_id                  = var.project_id
-  enable_apis                 = var.enable
-  disable_services_on_destroy = true
-
-  activate_apis = [
-    "sqladmin.googleapis.com",
-    "bigquery-json.googleapis.com",
-    "serviceusage.googleapis.com",
-  ]
-  activate_api_identities = [{
-    api = "serviceusage.googleapis.com"
-    roles = [
-      "roles/serviceusage.serviceUsageAdmin",
-    ]
-  }]
+  variable "enable" {
+  description = "Actually enable the APIs listed"
+  default     = true
+  
+}
 }
 
 
