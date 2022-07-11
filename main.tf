aa2627638521179fba9616f498f57dfc0362525f
amitn16 <amitnmj@gmail.com>
2022-07-11 14:38:37 +0200

    ok

--- main.tf
+++ main.tf
@@ -18,6 +18,39 @@ resource "google_binary_authorization_attestor" "attestor" {
   }
 }
 
+data "google_kms_crypto_key_version" "version" {
+  crypto_key = google_kms_crypto_key.crypto-key.id
+}
+
+resource "google_container_analysis_note" "note" {
+  name = "test-attestor-note"
+  attestation_authority {
+    hint {
+      human_readable_name = "Attestor Note"
+    }
+  }
+}
+
+resource "google_kms_crypto_key" "crypto-key" {
+  name     = "test-attestor-key"
+  key_ring = google_kms_key_ring.keyring.id
+  purpose  = "ASYMMETRIC_SIGN"
+
+  version_template {
+    algorithm = "RSA_SIGN_PKCS1_4096_SHA512"
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "google_kms_key_ring" "keyring" {
+  name     = "test-attestor-key-ring"
+  location = "global"
+}
+
+###########################################################
 data "google_iam_policy" "admin" {
   binding {
     role = "roles/viewer"
