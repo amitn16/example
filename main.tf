f1e4f552ac148fd3c38b76604f243ebb061288a7
amitn16 <amitnmj@gmail.com>
2022-07-11 13:54:59 +0200

    ok

--- main.tf
+++ main.tf
@@ -18,34 +18,17 @@ resource "google_binary_authorization_attestor" "attestor" {
   }
 }
 
-data "google_kms_crypto_key_version" "version" {
-  crypto_key = google_kms_crypto_key.crypto-key.id
-}
-
-resource "google_container_analysis_note" "note" {
-  name = "test-attestor-note"
-  attestation_authority {
-    hint {
-      human_readable_name = "Attestor Note"
-    }
+data "google_iam_policy" "admin" {
+  binding {
+    role = "roles/viewer"
+    members = [
+      "user:amit@bruttech.com",
+    ]
   }
 }
 
-resource "google_kms_crypto_key" "crypto-key" {
-  name     = "test-attestor-key"
-  key_ring = google_kms_key_ring.keyring.id
-  purpose  = "ASYMMETRIC_SIGN"
-
-  version_template {
-    algorithm = "RSA_SIGN_PKCS1_4096_SHA512"
-  }
-
-  lifecycle {
-    prevent_destroy = true
-  }
-}
-
-resource "google_kms_key_ring" "keyring" {
-  name     = "test-attestor-key-ring"
-  location = "global"
+resource "google_binary_authorization_attestor_iam_policy" "policy" {
+  project = google_binary_authorization_attestor.attestor.project
+  attestor = google_binary_authorization_attestor.attestor.name
+  policy_data = data.google_iam_policy.admin.policy_data
 }
