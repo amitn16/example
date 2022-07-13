5899445048835ebd000bbc077230348155a0e6d1
amitn16 <amitnmj@gmail.com>
2022-07-13 20:25:46 +0200

    ok

--- variables.tf
+++ variables.tf
@@ -1,20 +1,3 @@
-/**
- * Copyright 2019 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-
 # define GCP project name
 variable "project" {
   type = string
@@ -33,11 +16,4 @@ variable "zone" {
   type = string
   description = "GCP zone"
   default     = "us-central1"
-}
-
-# define private subnet
-variable "private_subnet_cidr_1" {
-  type = string
-  description = "private_subnet_CIDR 1"
-  default     = "10.10.1.0/24"
 }
