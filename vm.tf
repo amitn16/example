ae5798f4c4e98a81ae6d40f60c3e86f7eeafc289
amitn16 <amitnmj@gmail.com>
2022-07-14 19:12:51 +0200

    LB with module

--- vm.tf
+++ vm.tf
@@ -37,22 +37,3 @@ network_interface {
     subnetwork = google_compute_subnetwork.private_subnet_1.name
   }
 }
-
-# Create web server #3
-resource "google_compute_instance" "web_private_3" {
-  name = "${var.app_name}-vm3"
-  machine_type = "e2-micro"
-  zone = var.zone
-  hostname = "${var.app_name}-vm2.${var.app_domain}"
-  tags = ["ssh","http"]
-boot_disk {
-    initialize_params {
-      image = "ubuntu-os-cloud/ubuntu-1804-lts"
-    }
-  }
- network_interface {
-    network = google_compute_network.vpc.name
-    subnetwork = google_compute_subnetwork.private_subnet_1.name
-  }
-
-}
