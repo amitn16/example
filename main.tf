c5830633af3f631724bed20caba1651cd0dfb821
amitn16 <amitnmj@gmail.com>
2022-07-15 22:43:07 +0200

    ok

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ module "network" {
 
 module "vm" {
   source  = "app.terraform.io/Bruttech/vm/google"
-  version = "1.0.3"
+  version = "1.0.4"
   # insert required variables here
   depends_on = [module.network]
   app = var.app
