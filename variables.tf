889eb0b0b9c1bf939d53fef5df9df06c9d0b808b
amitn16 <amitnmj@gmail.com>
2022-07-15 15:31:19 +0200

    ok

--- variables.tf
+++ variables.tf
@@ -1,40 +1,49 @@
-/**
- * Copyright 2019 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
+# define GCP project name
 variable "project" {
   type = string
+  description = "GCP project name"
   default = "lithe-bonito-356116"
 }
 
-variable "target_size" {
-  type    = number
-  default = 2
+# define GCP region
+variable "region" {
+  type = string
+  description = "GCP region"
+  default     = "us-central"
+}
+# define GCP zone
+variable "zone" {
+  type = string
+  description = "GCP zone"
+  default     = "us-central1"
 }
 
-variable "group1_region" {
-  type    = string
-  default = "us-west1"
+# define private subnet
+variable "private_subnet_cidr_1" {
+  type = string
+  description = "private_subnet_CIDR 1"
+  default     = "10.10.1.0/24"
 }
 
-variable "group2_region" {
-  type    = string
-  default = "us-east1"
+# define application name
+variable "app_name" {
+  type = string
+  description = "Application name"
 }
 
-variable "network_prefix" {
-  type    = string
-  default = "multi-mig-lb-http"
+# define application domain
+variable "app_domain" {
+  type = string
+  description = "Application domain"
+}
+
+# define application environment
+variable "app_environment" {
+  type = string
+  description = "Application environment"
+}
+
+variable "app_node_count" {
+  type = string
+  description = "Number of servers to build"
 }
