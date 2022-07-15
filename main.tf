34acc227fe27f355207c92036b751953ee78ad14
amitn16 <amitnmj@gmail.com>
2022-07-15 20:06:25 +0200

    ok

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 module "network" {
   source  = "app.terraform.io/Bruttech/network/google"
   version = "2.0.3"
-  app_name = "var.app_name"
+  app_name = var.app_name
   private_subnet_cidr_1 = var.private_subnet_cidr_1
   project = var.project
   region = var.region
