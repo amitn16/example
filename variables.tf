0490c11b94e3234afcd6631618c71793cdf26d08
amitn16 <amitnmj@gmail.com>
2022-07-16 09:23:34 +0200

    ok

--- variables.tf
+++ variables.tf
@@ -26,26 +26,3 @@ variable "private_subnet_cidr_1" {
 }
 
 #############
-
-# define application name
-variable "app_name" {
-  type = string
-  description = "Application name"
-}
-
-# define application domain
-variable "app_domain" {
-  type = string
-  description = "Application domain"
-}
-
-# define application environment
-variable "app_environment" {
-  type = string
-  description = "Application environment"
-}
-
-variable "app_node_count" {
-  type = string
-  description = "Number of servers to build"
-}
