52a72409670c07dacb9390f4d00a9b36182384fc
amitn16 <amitnmj@gmail.com>
2022-07-16 09:21:09 +0200

    ok

--- main.tf
+++ main.tf
@@ -15,16 +15,22 @@ module "network" {
   zone = var.zone
 }
 
-module "vm" {
-  source  = "app.terraform.io/Bruttech/vm/google"
-  version = "1.0.5"
-  # insert required variables here
-  depends_on = [module.network]
-  app = var.app
-  private_subnet_cidr_1 = var.private_subnet_cidr_1
-  project = var.project
-  region = var.region
+# Create Google Cloud VMs | vm.tf
+# Create web server #1
+resource "google_compute_instance" "web_private_1" {
+  name = "${var.app_name}-vm1"
+  machine_type = "e2-micro"
   zone = var.zone
-  image = var.image
-  machine_type = var.machine_type
+  hostname = "${var.app_name}-vm1.${var.app_domain}"
+  tags = ["ssh","http"]
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-1804-lts"
+    }
+  }
+  
+  metadata_startup_script = "sudo apt-get update;sudo apt-get install -yq build-essential apache2"
+  network_interface {
+    network = google_compute_network.vpc.name
+    subnetwork = google_compute_subnetwork.private_subnet_1.name
   }
